7bab0278563d98c658084e3781fa6e9bfd76e850
ATLATcat <ATLATCat@users.noreply.github.com>
2022-12-25 16:56:10 +0900

    타임라인 Text Switcher 추가

---
+++ Assets/DefaultPlayables/TextSwitcher/TextSwitcherBehaviour.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
+using TMPro;
+using Febucci.UI;
+
+[Serializable]
+public class TextSwitcherBehaviour : PlayableBehaviour
+{
+    //public Color color = Color.white;
+    //public int fontSize = 14;
+    public string text;
+
+    bool isFirstFrame = true;
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        base.OnBehaviourPlay(playable, info);
+        TextMeshProUGUI m_TrackBinding = playerData as TextMeshProUGUI;
+
+        if(isFirstFrame)
+        {
+            m_TrackBinding.text = text;
+            if (m_TrackBinding.GetComponent<TextAnimatorPlayer>() is TextAnimatorPlayer textAnimatorPlayer)
+            {
+                textAnimatorPlayer.StartShowingText(true);
+            }
+
+            isFirstFrame = false;
+        }
+    }
+}
